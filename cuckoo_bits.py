955916ec7b0b6c1089ce964013ac2dff68546b7f
Luke Wallin <luke@lukewallin.co.uk>
2021-10-24 20:51:44 +0100

    made the numerals a bit thinner so they print much better, and meddled with the centre of the dial

--- cuckoo_bits.py
+++ cuckoo_bits.py
@@ -156,25 +156,24 @@ def cuckoo_back(width=88,length=120, edge_thick=2.2,inner_width=82,hole_d=10):
     return back
 
 def roman_numerals(number, height, workplane, thick=0.4):
-    width = height * 0.15
-    end_tip_height = width * 0.5
+    width = height * 0.1
+    #I changed the width later.. bodge
+    width_for_calcs = height*0.15
+    end_tip_height = width_for_calcs * 0.5
     # the sticky out bits
-    diamond_width = width * 2.5#2#1.75
-    diamond_height = width * 1#0.75
+    diamond_width = width_for_calcs * 2.5#2#1.75
+    diamond_height = width_for_calcs * 1#0.75
     #instead of a point, end in a blunt bit this tall
     # diamond_thinnest = diamond_height*0.1
-    v_top_width = width * 2.5
-    thin_width = width * 0.75
+    v_top_width = width_for_calcs * 2.5
+    thin_width = width_for_calcs * 0.75
     widths={}
-    widths["I"] = width*1.35# + diamond_width*0.175#0.2
-    widths["V"] = (v_top_width-width + diamond_width)*0.8
+    widths["I"] = width_for_calcs*1.35# + diamond_width*0.175#0.2
+    widths["V"] = (v_top_width-width_for_calcs + diamond_width)*0.8
     widths["X"] = widths["V"]
 
     def add_diamond(workplane,pos):
         return workplane.moveTo(pos[0],pos[1]).move(0,-diamond_height/2).line(diamond_width/2,diamond_height/2).line(-diamond_width/2,diamond_height/2).line(-diamond_width/2,-diamond_height/2).close().extrude(thick)
-
-    #the height of the taper at top and bottom
-    # taperHeight=width*math.sqrt(2)/2
     def make_i(workplane):
 
 
@@ -235,17 +234,9 @@ def roman_numerals(number, height, workplane, thick=0.4):
     total_width=0
     for char in number:
         total_width+=widths[char]
-    # current_x=0
-    #start off to the left by half the total width and half the first char width
-    # workplane=workplane.transformed(offset=(-total_width/2,0))
     for char in number:
         thiswidth=widths[char]
-        # workplane.translate()
         workplane = makes[char](workplane.transformed(offset=(thiswidth/2,0))).translate((-thiswidth/2,0))
-        # workplane=workplane.transformed(offset=(thiswidth/2,0))
-        # current_x+=thiswidth
-    print("totalwidth: {}".format(total_width))
-    # return make_i(workplane,height,thick)
     return workplane
 
 def dial(diameter=60, hole_d=7, black=True):
@@ -259,44 +250,31 @@ def dial(diameter=60, hole_d=7, black=True):
     #fontY = -radius + fontsize*0.9
     fontY = -((radius - edge_thick + inner_radius)/2 )
     fontThick = 0.4
-    blackThick = 0.4 if black else 0.001 #bodge of 0.001 because cadquery won't allow a line of length 0
+    blackThick = 0.4
     # dial.faces(">Z").workplane().tag("numbers_base")
     thick_ridge1 = 0.3
     thick_ridge2=0.4
 
     circle = cq.Workplane("XY").circle(radius)
     dial = cq.Workplane("XZ").moveTo(hole_d/2,0).lineTo(radius,0).line(0,base_thick*0.5).line(-edge_thick*0.5,base_thick).tangentArcPoint((radius-edge_thick,base_thick),relative=False).\
-        line(0,-blackThick).lineTo(inner_radius,base_thick-blackThick).line(0,blackThick).tangentArcPoint((-base_thick*0.25,base_thick*0.25),relative=True).tangentArcPoint((-base_thick*0.25,base_thick*0.25),relative=True).\
+        lineTo(inner_radius,base_thick).tangentArcPoint((-base_thick*0.25,base_thick*0.25),relative=True).tangentArcPoint((-base_thick*0.25,base_thick*0.25),relative=True).\
         line(-base_thick*0.5,0).tangentArcPoint((-base_thick*0.25,-base_thick*0.25),relative=True).tangentArcPoint((-base_thick*0.25,-base_thick*0.25),relative=True).\
-        tangentArcPoint((-base_thick*0.25,base_thick*0.25),relative=True).tangentArcPoint((-base_thick*0.25,base_thick*0.25),relative=True).\
-        tangentArcPoint((-base_thick * thick_ridge1, -base_thick * thick_ridge1), relative=True).tangentArcPoint((-base_thick * thick_ridge2, -base_thick * thick_ridge2), relative=True). \
+        line(-base_thick*0.25,0).tangentArcPoint((-base_thick*0.25,base_thick*0.25),relative=True).tangentArcPoint((-base_thick*0.25,base_thick*0.25),relative=True).\
+        line(-base_thick*0.4,0).tangentArcPoint((-base_thick * thick_ridge1, -base_thick * thick_ridge1), relative=True).tangentArcPoint((-base_thick * thick_ridge2, -base_thick * thick_ridge2), relative=True). \
         tangentArcPoint((-base_thick * thick_ridge2, base_thick * thick_ridge2), relative=True).tangentArcPoint((-base_thick * thick_ridge1, base_thick * thick_ridge1), relative=True). \
-        tangentArcPoint((-base_thick * 0.25, -base_thick * 0.25), relative=True).tangentArcPoint((-base_thick * 0.25, -base_thick * 0.25), relative=True). \
+        line(-base_thick*0.5,0).tangentArcPoint((-base_thick * 0.25, -base_thick * 0.25), relative=True).tangentArcPoint((-base_thick * 0.25, -base_thick * 0.25), relative=True). \
         tangentArcPoint((-base_thick * 0.25, base_thick * 0.25), relative=True).tangentArcPoint((-base_thick * 0.25, base_thick * 0.25), relative=True). \
-        tangentArcPoint((-base_thick * 0.25, -base_thick * 0.25), relative=True).tangentArcPoint((-base_thick * 0.25, -base_thick * 0.25), relative=True). \
-        lineTo(hole_d/2,base_thick*1).lineTo(hole_d/2,0).close().sweep(circle)
+        line(-base_thick * 0.3, 0).tangentArcPoint((-base_thick * 0.125, -base_thick * 0.125), relative=True).tangentArcPoint((-base_thick * 0.125, -base_thick * 0.125), relative=True). \
+        lineTo(hole_d/2,base_thick*1.25).lineTo(hole_d/2,0).close().sweep(circle)
 
-    # dial = dial.add(profile)
-    numberscq_base = cq.Workplane("XY").transformed(offset=(0,0,base_thick)).tag("numbers_base")
-    # numberscq = cq.Workplane("XY")
     numberscqs = []
     numbers = ["XII", "I", "II", "III", "IIII", "V", "VI", "VII", "VIII", "IX", "X", "XI"]
-    # allNumbersObj=cq.Workplane("XY")
     for i,num in enumerate(numbers):
-        #Trajan
-        #Times New Roman
-        #Cambria
         angleRads = -i*(math.pi*2/12)-math.pi/2
         fontAngleDegs = 360*angleRads/(math.pi*2)+90
-        font="Cambria"
-        # font="Times New Roman"
-        # numberscq = numberscq.workplaneFromTagged("numbers_base").transformed(rotate=(0,0,fontAngleDegs),offset=(math.cos(angleRads)*fontY,math.sin(angleRads)*fontY)).text(txt=num,fontsize=fontsize, distance=fontThick, cut=False, combine=True, font=font, kind="bold")#.rotate(axisStartPoint=(0,0,0),axisEndPoint=(0,0,1),angleDegrees=i*360/12)
-        # numcq = roman_numerals(num, fontsize, numberscq_base.workplaneFromTagged("numbers_base").transformed(rotate=(0,0,fontAngleDegs),offset=(math.cos(angleRads)*fontY,math.sin(angleRads)*fontY)))
 
-        #.transformed(rotate=(0,0,fontAngleDegs),offset=(math.cos(angleRads)*fontY,math.sin(angleRads)*fontY, base_thick))
         numcq = roman_numerals(num, fontsize,cq.Workplane("XY"), fontThick).rotate((0,0,0),(0,0,1),fontAngleDegs).translate((math.cos(angleRads)*fontY,math.sin(angleRads)*fontY, base_thick))
         numberscqs.append(numcq)
-        # numberscq_base = numberscq_base.add(numcq)
 
     numberscq_base = numberscqs[0]
     for i in range(1,len(numberscqs)):
@@ -309,6 +287,8 @@ def dial(diameter=60, hole_d=7, black=True):
     if black:
         blackDisc = cq.Workplane("XY").transformed(offset=(0,0,base_thick-blackThick)).circle(radius-edge_thick).extrude(blackThick).circle(inner_radius).cutThruAll()
         out.append(blackDisc)
+        dial = dial.cut(blackDisc)
+        out[0] = dial
 
     return out
 
