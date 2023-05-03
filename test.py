01c94c87eb687c2986c5fada4f79abbf953cb006
Luke Wallin <luke@lukewallin.co.uk>
2023-05-03 18:06:29 +0100

    experiments with bevel gears

--- test.py
+++ test.py
@@ -452,24 +452,25 @@ if False:
 # dial = Dial(outside_d=200, style=DialStyle.TONY_THE_CLOCK)
 # show_object(dial.get_wire_to_arbor_fixer())
 
-lift=4
-drop=2
-lock=2
-escapement = AnchorEscapement(drop=drop, lift=lift, teeth=30, lock=lock, anchorTeeth=None, toothHeightFraction=0.2, toothTipAngle=5, toothBaseAngle=4, style=AnchorStyle.CURVED_MATCHING_WHEEL)
-pendulum = Pendulum(escapement, 0.225, anchorHoleD=3, anchorThick=12, nutMetricSize=3, crutchLength=0,handAvoiderInnerD=100,
-                          bobD=60, bobThick=10, useNylocForAnchor=False)
-bow_tie = BowTie(width=200*tony_the_clock["bow_tie_width"]/tony_the_clock["diameter"], bob_nut_width=pendulum.gapWidth, bob_nut_height=pendulum.gapHeight)
+if False:
+    lift=4
+    drop=2
+    lock=2
+    escapement = AnchorEscapement(drop=drop, lift=lift, teeth=30, lock=lock, anchorTeeth=None, toothHeightFraction=0.2, toothTipAngle=5, toothBaseAngle=4, style=AnchorStyle.CURVED_MATCHING_WHEEL)
+    pendulum = Pendulum(escapement, 0.225, anchorHoleD=3, anchorThick=12, nutMetricSize=3, crutchLength=0,handAvoiderInnerD=100,
+                              bobD=60, bobThick=10, useNylocForAnchor=False)
+    bow_tie = BowTie(width=200*tony_the_clock["bow_tie_width"]/tony_the_clock["diameter"], bob_nut_width=pendulum.gapWidth, bob_nut_height=pendulum.gapHeight)
 
-# show_object(bow.get_outline())
-# show_object(bow.get_red(),options={"color":"red"} )
-# show_object(bow.get_yellow(),options={"color":"yellow"} )
+    # show_object(bow.get_outline())
+    # show_object(bow.get_red(),options={"color":"red"} )
+    # show_object(bow.get_yellow(),options={"color":"yellow"} )
 
-cosmetics={"red": bow_tie.get_red(),
-           "yellow": bow_tie.get_yellow()}
+    cosmetics={"red": bow_tie.get_red(),
+               "yellow": bow_tie.get_yellow()}
 
-pretty_bob = ItemWithCosmetics(shape = pendulum.getBob(), name="bow_tie_bob", background_colour="black", cosmetics=cosmetics)
+    pretty_bob = ItemWithCosmetics(shape = pendulum.getBob(), name="bow_tie_bob", background_colour="black", cosmetics=cosmetics)
 
-show_object(pretty_bob.get_models())
+    show_object(pretty_bob.get_models())
 
 
 # text = cq.Workplane("XY").moveTo(0, 0).text("Testing", 10, LAYER_THICK, kind="bold")
@@ -558,16 +559,51 @@ show_object(pretty_bob.get_models())
 
 # show_object(getHandDemo())
 
-# motionWorks = MotionWorks(extra_height=10, style=GearStyle.CURVES, thick=3, compensateLooseArbour=True, compact=True, inset_at_base=MotionWorks.STANDARD_INSET_DEPTH)
-# moon = MoonPhaseComplication3D(motionWorks)
+motionWorks = MotionWorks(extra_height=10, style=GearStyle.CURVES, thick=3, compensateLooseArbour=False, compact=True)#, inset_at_base=MotionWorks.STANDARD_INSET_DEPTH)
+moon = MoonPhaseComplication3D(motionWorks)
 
-# print(1/moon.ratio)
-
-# bevels = BevelGearPair(module=1, gear_teeth=32, pinion_teeth=30, face_width=4, pressure_angle=20)
-# #
-# show_object(bevels.assemble())
+print(1/moon.ratio)
+face_width = 6
+bevels = BevelGearPair(module=1, gear_teeth=32, pinion_teeth=30, face_width=face_width, pressure_angle=20)
 #
-# exporters.export(bevels.gear.build(), "out/test_bevel.stl")
+print(radToDeg(bevels.axis_angle), face_width*math.sin(bevels.axis_angle))
+
+# show_object(bevels.assemble())
+testbevel = cq.Workplane("XY").add(bevels.gear.build()).intersect(cq.Workplane("XY").rect(100,100).extrude(face_width*math.sin(bevels.axis_angle/2)).translate((0,0,0)))
+
+testbevel = testbevel.cut(cq.Workplane("XY").circle(3.2/2).extrude(10))
+
+testbevel_pinion = cq.Workplane("XY").add(bevels.pinion.build()).intersect(cq.Workplane("XY").rect(100,100).extrude(face_width*math.sin(bevels.axis_angle/2)))
+testbevel_pinion = testbevel_pinion.cut(cq.Workplane("XY").circle(3.2/2).extrude(10))
+
+show_object(testbevel)
+show_object(testbevel_pinion.rotate((0,0,0),(1,0,0),90).translate((0,bevels.pinion.cone_h,bevels.gear.cone_h)))
+# show_object(bevels.gear.build())
+# show_object(bevels.pinion.build())
+
+holder_wide = 10
+holder_thick = 5
+holder_long = bevels.pinion.cone_h + WASHER_THICK_M3
+
+holder_tall = bevels.gear.cone_h + holder_thick + WASHER_THICK_M3
+
+
+# testbevel_holder = cq.Workplane("XY").moveTo(0,holder_long/2 - holder_wide/2).rect(holder_wide, holder_long).extrude(holder_thick).faces(">Z").workplane().circle(3/2).cutThruAll()
+
+testbevel_holder = cq.Workplane("XY").circle(holder_wide/2).extrude(holder_thick)
+testbevel_holder = testbevel_holder.union(get_stroke_line([(0,0), (0,holder_long + holder_thick)],wide = holder_wide,thick = holder_thick, style=StrokeStyle.SQUARE))
+
+testbevel_holder = testbevel_holder.union(cq.Workplane("XY").moveTo(0,bevels.pinion.cone_h + WASHER_THICK_M3 + holder_thick/2).rect(holder_wide, holder_thick).extrude(holder_tall))
+testbevel_holder = testbevel_holder.union(cq.Workplane("XY").circle(holder_wide/2).extrude(holder_thick).rotate((0,0,0),(1,0,0),-90).translate((0,holder_long,holder_tall)))
+
+testbevel_holder = testbevel_holder.cut(cq.Workplane("XY").circle(3/2).extrude(holder_thick))
+testbevel_holder = testbevel_holder.cut(cq.Workplane("XY").circle(3/2).extrude(holder_thick).rotate((0,0,0),(1,0,0),-90).translate((0,holder_long,holder_tall)))
+
+show_object(testbevel_holder.translate((0,0,-holder_thick-WASHER_THICK_M3)))
+
+exporters.export(testbevel, "out/test_bevel.stl")
+exporters.export(testbevel_pinion, "out/test_bevel_pinion.stl")
+exporters.export(testbevel_holder, "out/test_bevel_holder.stl")
 #
 # mistletoe = MistletoeSprig() #MistletoeLeafPair()
 #
---
+++ wall_clock_23_moonphase.py
@@ -0,0 +1,94 @@
+from clocks import clock
+
+'''
+Clock 12 but without the dial, to see how it looks
+
+'''
+outputSTL = False
+
+if 'show_object' not in globals():
+    #don't output STL when we're in cadquery editor
+    outputSTL = True
+    def show_object(*args, **kwargs):
+        pass
+
+clockName="wall_clock_20"
+clockOutDir="out"
+gearStyle=clock.GearStyle.ARCS
+pendulumFixing=clock.PendulumFixing.DIRECT_ARBOUR_SMALL_BEARINGS
+
+#for period 1.5
+drop =1.5
+lift =3
+lock=1.5
+escapement = clock.AnchorEscapement(drop=drop, lift=lift, teeth=40, lock=lock, anchorTeeth=None, toothHeightFraction=0.2, toothTipAngle=5, toothBaseAngle=4, style=clock.AnchorStyle.CURVED_MATCHING_WHEEL)
+# lift=4
+# drop=2
+# lock=2
+# escapement = clock.AnchorEscapement(drop=drop, lift=lift, teeth=30, lock=lock, anchorTeeth=None, toothHeightFraction=0.2, toothTipAngle=5, toothBaseAngle=4)
+
+train = clock.GoingTrain(pendulum_period=1.5, fourth_wheel=False, escapement=escapement, maxWeightDrop=1000, usePulley=True, chainAtBack=False, chainWheels=1, hours=7.5*24)#, huygensMaintainingPower=True)
+
+moduleReduction=0.85
+
+train.calculateRatios(max_wheel_teeth=130, min_pinion_teeth=9, wheel_min_teeth=60, pinion_max_teeth=15, max_error=0.1, moduleReduction=moduleReduction)
+
+#think this is promising for good compromise of size
+train.genCordWheels(ratchetThick=4, rodMetricThread=4, cordThick=1, cordCoilThick=14, style=gearStyle, useKey=True, preferedDiameter=29, looseOnRod=False, prefer_small=True)
+# train.genChainWheels2(clock.COUSINS_1_5MM_CHAIN, ratchetThick=6, arbourD=4, looseOnRod=False, prefer_small=True, preferedDiameter=25, fixing_screws=clock.MachineScrew(3, countersunk=True),ratchetOuterThick=6)
+
+
+
+pendulumSticksOut=20
+
+train.genGears(module_size=1, moduleReduction=moduleReduction, thick=2.4, thicknessReduction=0.9, chainWheelThick=4, pinionThickMultiplier=3, style=gearStyle,
+               chainModuleIncrease=1, chainWheelPinionThickMultiplier=2, pendulumFixing=pendulumFixing)
+train.printInfo(weight_kg=3)
+train.getArbourWithConventionalNaming(0).printScrewLength()
+
+#although I can make really compact motion works now for the dial to be close, this results in a key that looks too short, so extending just so the key might be more stable
+motionWorks = clock.MotionWorks(extra_height=10, style=gearStyle, thick=3, compensateLooseArbour=True, compact=True, inset_at_base=clock.MotionWorks.STANDARD_INSET_DEPTH)
+#slightly larger allows for the inset and thus dial and hands closer to the plate
+motionWorks.calculateGears(arbourDistance=30)
+
+pendulum = clock.Pendulum(train.escapement, train.pendulum_length, anchorHoleD=3, anchorThick=12, nutMetricSize=3, crutchLength=0,handAvoiderInnerD=100,
+                          bobD=60, bobThick=10, useNylocForAnchor=False)
+
+dial = clock.Dial(outside_d=180, bottom_fixing=True, top_fixing=True, style=clock.DialStyle.ROMAN, seconds_style=clock.DialStyle.LINES_ARC)
+
+#dial diameter of 250 (printed in two parts) looks promising for second hand, 205 without
+plates = clock.SimpleClockPlates(train, motionWorks, pendulum, plateThick=9, backPlateThick=11, pendulumSticksOut=pendulumSticksOut, name="Wall 20",style=clock.ClockPlateStyle.COMPACT,
+                                 motionWorksAbove=True, heavy=True, extraHeavy=True, pendulumFixing=pendulumFixing, pendulumAtFront=False,
+                                 backPlateFromWall=pendulumSticksOut*2, fixingScrews=clock.MachineScrew(metric_thread=4, countersunk=True),
+                                 chainThroughPillarRequired=True, pillars_separate=True, dial=dial, bottom_pillars=2)
+
+
+# hands = clock.Hands(style=clock.HandStyle.SPADE, minuteFixing="square", minuteFixing_d1=motionWorks.getMinuteHandSquareSize(), hourfixing_d=motionWorks.getHourHandHoleD(),
+#                     length=plates.dial_diameter*0.45, thick=motionWorks.minuteHandSlotHeight, outline=1, outlineSameAsBody=False, secondLength=plates.second_hand_mini_dial_d*0.45)
+hands = clock.Hands(style=clock.HandStyle.BAROQUE,  minuteFixing="square",  minuteFixing_d1=motionWorks.getMinuteHandSquareSize(), hourfixing_d=motionWorks.getHourHandHoleD(),
+                    length=dial.outside_d*0.45, thick=motionWorks.minuteHandSlotHeight, outline=0, outlineSameAsBody=False, chunky=True, secondLength=dial.second_hand_mini_dial_d*0.45, seconds_hand_thick=1.5)
+# hands = clock.Hands(style="cuckoo", minuteFixing="square", minuteFixing_d1=motionWorks.getMinuteHandSquareSize(), hourfixing_d=motionWorks.getHourHandHoleD(), length=60, thick=motionWorks.minuteHandSlotHeight, outlineSameAsBody=False)
+
+pulley = clock.BearingPulley(diameter=train.poweredWheel.diameter, bearing=clock.getBearingInfo(4), wheel_screws=clock.MachineScrew(2, countersunk=True, length=8))
+print("pulley needs screws {} {}mm and {} {}mm".format(pulley.screws, pulley.getTotalThick(), pulley.hook_screws, pulley.getHookTotalThick()))
+
+
+assembly = clock.Assembly(plates, hands=hands, timeSeconds=30, pulley = pulley)#weights=[clock.Weight(height=245,diameter=55)]
+
+# show_object(plates.getPlate(back=True))
+show_object(assembly.getClock(with_key=False, with_pendulum=True))
+
+# show_object(plates.getDrillTemplate(6))
+
+if outputSTL:
+    #
+    #
+    train.outputSTLs(clockName,clockOutDir)
+    motionWorks.outputSTLs(clockName,clockOutDir)
+    pendulum.outputSTLs(clockName, clockOutDir)
+    plates.outputSTLs(clockName, clockOutDir)
+    hands.outputSTLs(clockName, clockOutDir)
+    pulley.outputSTLs(clockName, clockOutDir)
+    assembly.outputSTLs(clockName, clockOutDir)
+
+    # clock.outputSTLMultithreaded([train, motionWorks,pendulum,dial,plates,hands,pulley,assembly], clockName, clockOutDir)
